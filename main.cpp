b0dfa0f4f82ace43a59dbdbdc06a2bb265c04f61
Tobias <tobias.garcia@udea.edu.co>
2020-05-12 17:55:15 -0500

    Mezclamos las ramas show con master

--- main.cpp
+++ main.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 #include "dialog.h"
 #include "login.h"
-#include "show.h"
 //#include "code_decode.h"
 
 using namespace std;
 
 int main() {
 
-    vector<Show> shows;
-
-    cout << get_shows(shows) << endl;
-
-//    bool exists;
-//    Show show(0, exists);
-
-//    show.display_seats(false);
-//    show.save_show(0);
-
-//    room.display_seats(true);
-//    room.save_room();
-
-
     //code_file("../DesafioEvaluativo/data/nat.txt", "../DesafioEvaluativo/data/sudo.txt", 12);
 
-//    display_welcome();
+    display_welcome();
 
-////    La semilla de codificación por defecto es 12,
-////    pero puede ser cualquier otro entero positivo.
+//    La semilla de codificación por defecto es 12,
+//    pero puede ser cualquier otro entero positivo.
 
-//    unsigned int code_seed = 12;
-//    bool on = true, is_admin, logged;
+    unsigned int code_seed = 12;
+    bool on = true, is_admin, logged;
 
-//    while (on) {
-//        logged = login(code_seed, is_admin);
-//        if (logged and is_admin) {
-//            cout << endl << "  ADMIN LOGEUADO!" << endl << endl;
-//            cout << "  "; system("pause");
-//        }
-//        else if (logged) {
-//            cout << endl << "  USER LOGUEADO!" << endl << endl;
-//            cout << "  "; system("pause");
-//        }
-//        display_welcome();
-//    }
+    while (on) {
+        logged = login(code_seed, is_admin);
+        if (logged and is_admin) {
+            cout << endl << "  ADMIN LOGEUADO!" << endl << endl;
+            cout << "  "; system("pause");
+        }
+        else if (logged) {
+            cout << endl << "  USER LOGUEADO!" << endl << endl;
+            cout << "  "; system("pause");
+        }
+        display_welcome();
+    }
 
     return 0;
 }
